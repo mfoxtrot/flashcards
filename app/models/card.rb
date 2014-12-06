f49cb0a5f45706d1e1a0614b5a5f238bea5f8997
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-07 03:52:22 +0400

    Lesson 3: Add validations for Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,13 @@
 class Card < ActiveRecord::Base
+  #validate presence
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  #validate unequalness
+  validate :unequalness
+
+  def unequalness
+    if original_text == translated_text
+      errors[:base] << "Оригинал и перевод не могут совпадать!"
+    end
+  end
 end
