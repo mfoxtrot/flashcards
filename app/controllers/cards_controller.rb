b5e64418b2d97f7ddbefe4c7174e14b65f1c8f3f
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-07 18:33:37 +0400

    Lesson 3: Add edit and show methods fot the card controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,8 +8,8 @@ class CardsController < ApplicationController
   end
 
   def create
-    @card_params = params[:card]
     @card = Card.new
+    @card_params = params[:card]
     @card.original_text = @card_params[:original_text]
     @card.translated_text = @card_params[:translated_text]
     @card.review_date = @card_params[:review_date]
@@ -21,5 +21,24 @@ class CardsController < ApplicationController
     @card = Card.find(params[:id])
     @card.delete
     redirect_to cards_path
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+    #@card.edit
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card_params = params[:card]
+    @card.original_text = @card_params[:original_text]
+    @card.translated_text = @card_params[:translated_text]
+    @card.review_date = @card_params[:review_date]
+    @card.save
+    redirect_to cards_path
+  end
+
+  def show
+    @card = Card.find(params[:id])
   end
 end
