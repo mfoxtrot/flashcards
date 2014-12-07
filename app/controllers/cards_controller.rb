1c6dba69a736b13dfd7ec19ec8792ba0a0eac68f
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-07 16:55:34 +0400

    Lesson 3: Add new/create methods

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,19 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card_params = params[:card]
+    @card = Card.new
+    @card.original_text = @card_params[:original_text]
+    @card.translated_text = @card_params[:translated_text]
+    @card.review_date = @card_params[:review_date]
+    @card.save
+    redirect_to cards_path
+  end
+
 end
