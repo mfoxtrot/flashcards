e8b3b0cba8b5872db0434771457d4b0ae0f7d73e
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-08 01:00:24 +0400

    Lesson 3: Fix some erros and DRY-optimization

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,6 @@
 class CardsController < ApplicationController
+  before_action :current_card, only: [:destroy, :edit, :update, :show]
+
   def index
     @cards = Card.all
   end
@@ -14,31 +16,29 @@ class CardsController < ApplicationController
   end
 
   def destroy
-    @card = Card.find(params[:id])
     @card.delete
     redirect_to cards_path
   end
 
   def edit
-    @card = Card.find(params[:id])
   end
 
   def update
-    @card = Card.find(params[:id])
-    @card.original_text = card_params[:original_text]
-    @card.translated_text = card_params[:translated_text]
-    @card.review_date = card_params[:review_date]
+    @card.update(card_params)
     @card.save
     redirect_to cards_path
   end
 
   def show
-    @card = Card.find(params[:id])
   end
 
   private
 
   def card_params
     params.require(:card).permit(:original_text, :translated_text, :review_date)
+  end
+
+  def current_card
+    @card = Card.find(params[:id])
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,8 @@
 class Card < ActiveRecord::Base
-  validates :original_text, presence: true
-  validates :translated_text, presence: true
-  validate :unequalness
+  validates :original_text, :translated_text, presence: true
+  validate :unequal
 
-  def unequalness
+  def unequal
     if original_text == translated_text
       errors[:base] << "Оригинал и перевод не могут совпадать!"
     end
