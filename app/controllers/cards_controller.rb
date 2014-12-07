e2c756a35223ddfb691e492d27fd1bc9ec714702
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-07 18:51:31 +0400

    Lesson 3: Make some refactoring (private method card_params)

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,11 +8,7 @@ class CardsController < ApplicationController
   end
 
   def create
-    @card = Card.new
-    @card_params = params[:card]
-    @card.original_text = @card_params[:original_text]
-    @card.translated_text = @card_params[:translated_text]
-    @card.review_date = @card_params[:review_date]
+    @card = Card.new(card_params)
     @card.save
     redirect_to cards_path
   end
@@ -25,20 +21,23 @@ class CardsController < ApplicationController
 
   def edit
     @card = Card.find(params[:id])
-    #@card.edit
   end
 
   def update
     @card = Card.find(params[:id])
-    @card_params = params[:card]
-    @card.original_text = @card_params[:original_text]
-    @card.translated_text = @card_params[:translated_text]
-    @card.review_date = @card_params[:review_date]
+    @card.original_text = card_params[:original_text]
+    @card.translated_text = card_params[:translated_text]
+    @card.review_date = card_params[:review_date]
     @card.save
     redirect_to cards_path
   end
 
   def show
     @card = Card.find(params[:id])
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
   end
 end
