38296e23f89dbe9d2752937a057fd0cbd1205630
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-24 21:31:08 +0300

    Lesson 6: Fix some errors

--- spec/features/card_spec_capybara.rb
+++ spec/features/card_spec_capybara.rb
@@ -4,21 +4,29 @@ include Capybara::DSL
 describe "The main page: ", js: true do
 
   it "Opens homepage" do
-    visit(root_path)
+    visit root_path
     expect(page).to have_content "Первый в мире удобный менеджер флеш-карточек. Именно так."
   end
 
   it "Main page shows the original text to check translation" do
     card = FactoryGirl.create(:card)
-    visit(root_path)
+    visit root_path
     expect(page).to have_content card.original_text
   end
 
   it "User's able to check the translation" do
     card = FactoryGirl.create(:card)
-    visit(root_path)
+    visit root_path
     fill_in "answer", with: card.translated_text
     click_button "Проверить"
     expect(page).to have_content "Правильно"
+  end
+
+  it "Returns error when user's translation is incorrect" do
+    card = FactoryGirl.create(:card)
+    visit root_path
+    fill_in "answer", with: ('a'..'z').to_a.shuffle.first(rand(26)).join
+    click_button "Проверить"
+    expect(page).to have_content "Не правильно"
   end
 end
--- spec/features/card_spec_phantom.rb
+++ spec/features/card_spec_phantom.rb
@@ -3,15 +3,9 @@ require 'capybara/poltergeist'
 
 describe 'Test the main page', js: true do
 
-  it 'Make a screenshot' do
-    card = FactoryGirl.create(:card)
-    visit(root_path)
-    page.save_screenshot('tmp/screenshots/main_page.png', full: true)
-  end
-
   it 'Main page shoes the original text to check the translation' do
     card = FactoryGirl.create(:card)
-    visit(root_path)
+    visit root_path
     expect(page).to have_content card.original_text
   end
 end
