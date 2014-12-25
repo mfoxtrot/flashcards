3ff56d1bede65763a37ce06ab52c513a313d67dd
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-25 23:06:08 +0300

    Lesson 6: Modify capybara test with 'context'

--- spec/features/card_spec_capybara.rb
+++ spec/features/card_spec_capybara.rb
@@ -3,30 +3,36 @@ include Capybara::DSL
 
 describe "The main page: ", js: true do
 
-  it "Opens homepage" do
+  it "Has slogan" do
     visit root_path
     expect(page).to have_content "Первый в мире удобный менеджер флеш-карточек. Именно так."
   end
 
-  it "Main page shows the original text to check translation" do
-    card = FactoryGirl.create(:card)
-    visit root_path
-    expect(page).to have_content card.original_text
-  end
+  describe "Allows to test translation and " do
 
-  it "User's able to check the translation" do
-    card = FactoryGirl.create(:card)
-    visit root_path
-    fill_in "answer", with: card.translated_text
-    click_button "Проверить"
-    expect(page).to have_content "Правильно"
-  end
+    before(:each) {
+        @card = FactoryGirl.create(:card)
+        visit root_path
+      }
 
-  it "Returns error when user's translation is incorrect" do
-    card = FactoryGirl.create(:card)
-    visit root_path
-    fill_in "answer", with: ('a'..'z').to_a.shuffle.first(rand(26)).join
-    click_button "Проверить"
-    expect(page).to have_content "Не правильно"
+    subject { page }
+    context do
+
+      it "Shows the original text to check translation" do
+        is_expected.to have_content @card.original_text
+      end
+
+      it "Returns OK when the translation is correct" do
+        fill_in "answer", with: @card.translated_text
+        click_button "Проверить"
+        is_expected.to have_content "Правильно"
+      end
+
+      it "Returns error when the translation is incorrect" do
+        fill_in "answer", with: 'mana-mana'
+        click_button "Проверить"
+        is_expected.to have_content "Не правильно"
+      end
+    end
   end
 end
