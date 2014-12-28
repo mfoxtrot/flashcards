a6ee39db957e84806d042faf94710f022951b948
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-28 19:53:38 +0300

    Lesson 7: Add some user tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141225225603) do
+ActiveRecord::Schema.define(version: 20141225233013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20141225225603) do
     t.date     "review_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
+    t.integer  "user_id",         null: false
   end
 
-  add_index "cards", ["user_id"], name: "index_cards_on_user_id", unique: true, using: :btree
+  add_index "cards", ["user_id"], name: "index_cards_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe User do
+
+  it "Saves new user when all fields are correct" do
+    new_user = User.new(email: "johndoe@gmail.com", password: "1234567890")
+    expect(new_user).to be_valid
+  end
+
+  it "Is not valid when email doesn't present" do
+    new_user = User.new(password: "1234567890")
+    expect(new_user).not_to be_valid
+  end
+
+  it "Is not valid when password doesn't present" do
+    new_user = User.new(email: "johndoe@gmail.com")
+    expect(new_user).not_to be_valid
+  end
+
+  it "Is not valid when password's length less than 1" do
+    new_user = User.new(email: "johndoe@gmail.com", password: "")
+    expect(new_user).not_to be_valid
+  end
+
+  it "Is not valid when entered email doesn't match email format" do
+    new_user = User.new(email: "johndoe@gmail", password: "12345")
+    expect(new_user).not_to be_valid
+  end
+
+  it "Is able to have many cards" do
+    new_user = User.new(email: "johndoe@gmail.com", password: "12345")
+    new_user.save
+    new_card1 = Card.new(original_text: "hi", translated_text: "привет", review_date: "01.01.2014", user: new_user)
+    new_card1.save
+    new_card2 = Card.new(original_text: "bye", translated_text: "пока", review_date: "01.01.2014", user: new_user)
+    new_card2.save
+    expect(new_user.cards.count).to eq(2)
+  end
+end
