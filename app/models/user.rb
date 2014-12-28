3c2739035144ee635d67d761e939bcfbbaa1af91
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-28 23:54:19 +0300

    Lesson 8: Add UserSessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  validates :email, uniqueness: true
+  validates_email_format_of :email, message: "Некорректный адрес эл.почты"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Flashcards::Application.routes.draw do
+  get "user_sessions/new"
+  get "user_sessions/create"
+  get "user_sessions/destroy"
   root 'home#index'
   resources :cards
   resources :users
