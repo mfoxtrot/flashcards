f140bb734d1bdc2ca5831a49034dd358564d24b6
mfoxtrot <dmitrey.sokolov@gmial.com>
2014-12-31 01:15:18 +0300

    Lesson 8: Fix models tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
 class User < ActiveRecord::Base
-  #  attr_accessible :email, :password, :password_confirmation, :authentications_attributes
-  attr_accessor :external
 
   authenticates_with_sorcery! do |config|
     config.authentications_class = Authentication
   end
 
-  validates :password, length: { minimum: 3 }, unless: -> { @external }
-  validates :password, confirmation: true, unless: -> { @external }
-  validates :password_confirmation, presence: true, unless: -> { @external }
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 
   validates :email, uniqueness: true
-  validates_email_format_of :email, message: "Некорректный адрес эл.почты", unless: -> { @external }
+  validates_email_format_of :email, message: "Некорректный адрес эл.почты"
 
   has_many :cards
 
